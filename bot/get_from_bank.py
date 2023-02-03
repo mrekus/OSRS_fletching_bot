1f8b612f285b56c52d18fe98027db669a87c2266
Marius R <marius.rekus@gmail.com>
2023-02-03 21:48:01 +0200

    Surašytos inventory.py f-jos

--- bot/get_from_bank.py
+++ bot/get_from_bank.py
@@ -5,17 +5,17 @@ import time
 
 def click_grapes_bank(x=968, y=120):
     win32api.SetCursorPos((x, y))
-    time.sleep(0.5)
+    time.sleep(0.47)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    time.sleep(0.1)
+    time.sleep(0.09)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-    time.sleep(0.1)
+    time.sleep(0.12)
 
 
 def click_jug_bank(x=1016, y=120):
     win32api.SetCursorPos((x, y))
-    time.sleep(0.5)
+    time.sleep(0.53)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
     time.sleep(0.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-    time.sleep(0.1)
+    time.sleep(0.11)
--- bot/inventory.py
+++ bot/inventory.py
@@ -3,10 +3,19 @@ from utils import win32con
 import time
 
 
-def click_grapes(x=968, y=120):
+def click_grapes_inventory(x=1457, y=357):
     win32api.SetCursorPos((x, y))
-    time.sleep(0.5)
+    time.sleep(0.49)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
     time.sleep(0.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+    time.sleep(0.11)
+
+
+def click_jug_inventory(x=1458, y=394):
+    win32api.SetCursorPos((x, y))
+    time.sleep(0.51)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
     time.sleep(0.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+    time.sleep(0.12)
