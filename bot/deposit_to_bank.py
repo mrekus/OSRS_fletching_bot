18b15b0d8ff5b7715ad51bac5f2ee8c1d2b5b6ff
Marius R <marius.rekus@gmail.com>
2023-02-03 21:59:36 +0200

    Pabaigtas botas, prijugntas begalinis ciklas

--- bot/deposit_to_bank.py
+++ bot/deposit_to_bank.py
@@ -3,19 +3,19 @@ from utils import win32con
 import time
 
 
-def click_grapes_inventory(x=1457, y=357):
+def click_bank(x=1267, y=323):
     win32api.SetCursorPos((x, y))
-    time.sleep(0.49)
+    time.sleep(0.45)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
     time.sleep(0.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
     time.sleep(0.11)
 
 
-def click_jug_inventory(x=1458, y=394):
+def deposit_wine(x=1322, y=336):
     win32api.SetCursorPos((x, y))
-    time.sleep(0.7)
+    time.sleep(0.45)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
     time.sleep(0.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-    time.sleep(0.5)
+    time.sleep(0.11)
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from bot.get_from_bank import click_grapes_bank, click_jug_bank
 from bot.inventory import click_grapes_inventory, click_jug_inventory
+from bot.deposit_to_bank import click_bank, deposit_wine
 from utils import pyautogui
 import time
 
 
 def main():
-    click_grapes_bank()
-    click_jug_bank()
-    pyautogui.press("esc")
-    click_grapes_inventory()
-    click_jug_inventory()
-    pyautogui.press("space")
-    time.sleep(17)
+    while True:
+        time.sleep(1)
+        click_grapes_bank()
+        click_jug_bank()
+        pyautogui.press("esc")
+        click_grapes_inventory()
+        click_jug_inventory()
+        pyautogui.press("space")
+        time.sleep(18)
+        click_bank()
+        deposit_wine()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
