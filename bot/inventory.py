352731b336d46abc4bd35c9ea26137f90a5fa476
Marius R <marius.rekus@gmail.com>
2023-02-03 21:28:21 +0200

    Pridėti utils importai, sukurtas pirmas inventory.py failas

---
+++ bot/inventory.py
@@ -0,0 +1,10 @@
+from utils import *
+
+
+def click(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+
+
+click(10, 10)
