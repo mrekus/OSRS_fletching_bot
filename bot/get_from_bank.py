c28da6db98dc76d715847db24cf780ce4d76c31a
Marius R <marius.rekus@gmail.com>
2023-02-03 21:43:57 +0200

    Pabaigtas paėmimas iš banko

--- bot/get_from_bank.py
+++ bot/get_from_bank.py
@@ -1,14 +1,21 @@
 from utils import win32api
 from utils import win32con
+import time
 
 
 def click_grapes(x=968, y=120):
     win32api.SetCursorPos((x, y))
+    time.sleep(0.5)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+    time.sleep(0.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+    time.sleep(0.1)
 
 
 def click_jug(x=1016, y=120):
     win32api.SetCursorPos((x, y))
+    time.sleep(0.5)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+    time.sleep(0.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+    time.sleep(0.1)
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from bot.get_from_bank import click_grapes, click_jug
+from utils import pyautogui
+import time
+
+
 def main():
-    pass
+    click_grapes()
+    click_jug()
+    pyautogui.press("esc")
 
 
 if __name__ == "__main__":
+    time.sleep(5)
     main()
