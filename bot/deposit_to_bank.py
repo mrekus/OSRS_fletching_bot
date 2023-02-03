c54a7a9b9e814d85adbd6a15390b3943e757d7ca
Marius R <marius.rekus@gmail.com>
2023-02-04 01:15:30 +0200

    Pertvarkyti delays

--- bot/deposit_to_bank.py
+++ bot/deposit_to_bank.py
@@ -15,7 +15,7 @@ def click_bank(x=randint(1264, 1269), y=randint(321, 324)):
 
 def deposit_wine(x=randint(1320, 1324), y=randint(334, 337)):
     win32api.SetCursorPos((x, y))
-    time.sleep(round(uniform(0.7, 0.8), 2))
+    time.sleep(round(uniform(0.40, 0.49), 2))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
     time.sleep(round(uniform(0.09, 0.12), 2))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
--- bot/inventory.py
+++ bot/inventory.py
@@ -15,7 +15,7 @@ def click_grapes_inventory(x=randint(1455, 1459), y=randint(355, 358)):
 
 def click_jug_inventory(x=randint(1455, 1459), y=randint(391, 396)):
     win32api.SetCursorPos((x, y))
-    time.sleep(round(uniform(0.7, 0.8), 2))
+    time.sleep(round(uniform(0.40, 0.49), 2))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
     time.sleep(round(uniform(0.09, 0.12), 2))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
