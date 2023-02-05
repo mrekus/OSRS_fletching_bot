295c17748f55f59da5bea3379e8d83d990ae467f
Marius R <marius.rekus@gmail.com>
2023-02-05 17:09:03 +0200

    Pakeistos inventory.py koordinatės

--- bot/inventory.py
+++ bot/inventory.py
@@ -4,7 +4,7 @@ import time
 from random import randint, uniform
 
 
-def click_grapes_inventory(x=randint(1455, 1459), y=randint(355, 358)):
+def click_bow_inventory(x=randint(1130, 1142), y=randint(347, 362)):
     win32api.SetCursorPos((x, y))
     time.sleep(round(uniform(0.40, 0.49), 2))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
@@ -13,7 +13,7 @@ def click_grapes_inventory(x=randint(1455, 1459), y=randint(355, 358)):
     time.sleep(round(uniform(0.09, 0.12), 2))
 
 
-def click_jug_inventory(x=randint(1455, 1459), y=randint(391, 396)):
+def click_string_inventory(x=randint(1128, 1146), y=randint(387, 402)):
     win32api.SetCursorPos((x, y))
     time.sleep(round(uniform(0.40, 0.49), 2))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
