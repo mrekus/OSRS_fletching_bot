2570e8fb1404cd46a91dc051de418efa7e6c2b74
Marius R <marius.rekus@gmail.com>
2023-02-05 21:19:17 +0200

    Pakeisti delays

--- bot/get_from_bank.py
+++ bot/get_from_bank.py
@@ -6,13 +6,13 @@ from random import randint, uniform
 
 def click_bow_bank(x=randint(3196, 3212), y=randint(112, 129)):
     win32api.SetCursorPos((x, y))
-    time.sleep(round(uniform(0.40, 0.49), 2))
-    pyautogui.click(clicks=1, interval=(round(uniform(0.09, 0.12), 2)))
-    time.sleep(round(uniform(0.09, 0.12), 2))
+    time.sleep(round(uniform(0.2, 0.3), 2))
+    pyautogui.click(clicks=1, interval=(round(uniform(0.02, 0.05), 2)))
+    time.sleep(round(uniform(0.07, 0.10), 2))
 
 
 def click_string_bank(x=randint(3246, 3260), y=randint(112, 129)):
     win32api.SetCursorPos((x, y))
-    time.sleep(round(uniform(0.40, 0.49), 2))
-    pyautogui.click(clicks=1, interval=(round(uniform(0.09, 0.12), 2)))
-    time.sleep(round(uniform(0.09, 0.12), 2))
+    time.sleep(round(uniform(0.2, 0.3), 2))
+    pyautogui.click(clicks=1, interval=(round(uniform(0.02, 0.5), 2)))
+    time.sleep(round(uniform(0.07, 0.10), 2))
--- bot/inventory.py
+++ bot/inventory.py
@@ -6,13 +6,13 @@ from random import randint, uniform
 
 def click_bow_inventory(x=randint(3690, 3702), y=randint(347, 362)):
     win32api.SetCursorPos((x, y))
-    time.sleep(round(uniform(0.40, 0.49), 2))
-    pyautogui.click(clicks=1, interval=(round(uniform(0.09, 0.12), 2)))
-    time.sleep(round(uniform(0.09, 0.12), 2))
+    time.sleep(round(uniform(0.2, 0.3), 2))
+    pyautogui.click(clicks=1, interval=(round(uniform(0.02, 0.05), 2)))
+    time.sleep(round(uniform(0.07, 0.10), 2))
 
 
 def click_string_inventory(x=randint(3688, 3706), y=randint(387, 402)):
     win32api.SetCursorPos((x, y))
-    time.sleep(round(uniform(0.40, 0.49), 2))
-    pyautogui.click(clicks=1, interval=(round(uniform(0.09, 0.12), 2)))
-    time.sleep(round(uniform(0.09, 0.12), 2))
+    time.sleep(round(uniform(0.2, 0.3), 2))
+    pyautogui.click(clicks=1, interval=(round(uniform(0.02, 0.05), 2)))
+    time.sleep(round(uniform(0.07, 0.10), 2))
