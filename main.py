0b12018b98b82e107a4aba9ba0639a19042b4643
Marius R <marius.rekus@gmail.com>
2023-02-05 17:10:15 +0200

    Pakeistos main f-jos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from bot.get_from_bank import click_grapes_bank, click_jug_bank
-from bot.inventory import click_grapes_inventory, click_jug_inventory
-from bot.deposit_to_bank import click_bank, deposit_wine
+from bot.get_from_bank import click_string_bank, click_bow_bank
+from bot.inventory import click_bow_inventory, click_string_inventory
+from bot.deposit_to_bank import click_bank, deposit_bows
 import pyautogui
 import time
 from random import uniform
@@ -9,18 +9,18 @@ from random import uniform
 def main():
     while True:
         time.sleep(round(uniform(0.08, 0.12), 2))
-        click_grapes_bank()
-        click_jug_bank()
+        click_string_bank()
+        click_bow_bank()
         pyautogui.press("esc")
         time.sleep(round(uniform(0.08, 0.12), 2))
-        click_grapes_inventory()
-        click_jug_inventory()
+        click_bow_inventory()
+        click_string_inventory()
         time.sleep(round(uniform(0.7, 0.8), 2))
         pyautogui.press("space")
         time.sleep(round(uniform(17, 18), 2))
         click_bank()
         time.sleep(round(uniform(0.2, 0.3), 2))
-        deposit_wine()
+        deposit_bows()
         time.sleep(round(uniform(0.08, 0.12), 2))
 
 
